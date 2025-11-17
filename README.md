07459f85d0180b20b08ab4254cedb1e3611363cb
Ernest V <ernest.viol@gmail.com>
2025-11-17 15:25:10 -0800

    Add functionality
    Added the functionality to color in the nodes as you mouse over them.
    Added a reset colors button so that we're able to reset the grid without
    resetting the rows and columns.
    
    Unable to figure out how to account for a gap which is why we don't
    include a gap.

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ Create a container that contains all of the nodes up to 16
 Distribute equally each of the nodes height and width wise using some calculation
 - If 16 I want to go 4 x 4. So many we can do some sort of square root?
 - If 25 it should by 5 x 5? But we have more space width wise than height wise.
+
+
+Color the background of the node when we hover over it
+- Use a random color to update the background
+- When hover a background that has a color darken that color
+- Over time the color should lighten to white
+
+Implement a slider that can increase the number of nodes
+- Add event onSliderChange that updates the number of nodes
--- script.js
+++ script.js
@@ -1,9 +1,74 @@
-const INITIAL_NODE_COUNT = 16;
+// initialize the page with a number of nodes
+let currentNodeCount = 0;
 
-const node__container = document.querySelector('.node__container');
+const width = document.querySelector('#width');
+const height = document.querySelector('#height');
 
-for (let i = 0; i < INITIAL_NODE_COUNT; i++) {
+updateNodeCount(width.value*height.value);
+
+width.addEventListener('input', () => {
+  if (Number(width.value) > Number(width.max)) return;
+  updateNodeCount(width.value * height.value);
+  const root = document.documentElement;
+  root.style.setProperty('--width', width.value);
+})
+
+height.addEventListener('input', () => {
+  if (Number(height.value) > Number(height.max)) return;
+  updateNodeCount(width.value * height.value);
+  const root = document.documentElement;
+  root.style.setProperty('--height', height.value);
+})
+
+const button = document.querySelector('.reset');
+button.addEventListener('click',(event) => {
+  event.preventDefault();
+  resetColors();
+})
+
+function updateNodeCount(num) {
+  const nodeContainer = document.querySelector('.node__container');
+
+  if (num < currentNodeCount) {
+    for (let i = currentNodeCount; i > num; i--) {
+      nodeContainer.removeChild(nodeContainer.lastElementChild);
+    }
+  } else if (num > currentNodeCount) {
+    for (let i = currentNodeCount; i < num; i++) {
+      createNode(nodeContainer);
+    }
+  }
+  currentNodeCount = num;
+}
+
+function createNode(nodeContainer) {
   const node = document.createElement('div');
   node.classList.add('node')
-  node__container.appendChild(node);
+  node.addEventListener('mouseover', () => {
+    // get opacity
+    let r = g = b = a = 0;
+
+    if (node.style.backgroundColor) {
+      // increase opacity
+      const values = node.style.backgroundColor.replace(/^rgba?\(|\)$/g, '').split(',');
+      r = parseInt(values[0].trim()) - 10;
+      g = parseInt(values[1].trim()) - 10;
+      b = parseInt(values[2].trim()) - 10;
+      alpha = parseFloat(values[3] ? values[3].trim() : 1) + .2;
+    } else {
+      r = Math.floor(Math.random() * 255);
+      g = Math.floor(Math.random() * 255);
+      b = Math.floor(Math.random() * 255);
+      alpha = .4;
+    }
+    node.style.backgroundColor = `rgba(${r},${g},${b},${alpha})`;
+  })
+  nodeContainer.appendChild(node);
+};
+
+function resetColors() {
+  const nodes = document.querySelectorAll('.node');
+  nodes.forEach( node => {
+    node.style.backgroundColor = ''
+  })
 }
